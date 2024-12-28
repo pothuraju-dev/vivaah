5056bf0a10510f9534f2a2b472c99096a33993ff
Pothuraju <ram.k160796@gmail.com>
2024-12-28 11:19:55 -0600

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import { fileURLToPath, URL } from 'node:url'
+import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+
+import Components from 'unplugin-vue-components/vite'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter(),
+    Components({
+      /* options */
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      imports: [
+        'vue',
+        VueRouterAutoImports,
+        {
+          pinia: ['defineStore', 'storeToRefs', 'acceptHMRUpdate']
+        },
+        {
+          'vue-meta': ['useMeta']
+        }
+      ],
+      dts: true,
+      viteOptimizeDeps: true,
+      dirs: ['src/stores/**', 'src/composables/**']
+    }),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon')
+        }
+      }
+    })
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
